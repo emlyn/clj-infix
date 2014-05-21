a88f6e61c8b260223a315ba16db7a9ec82653397
Emlyn Corrin <emlyn@swiftkey.net>
2014-05-21 14:57:36 +0100

    cleanup

--- infix.clj
+++ infix.clj
@@ -7,9 +7,7 @@
   (:require [clojure.math.numeric-tower :refer :all])
   (:refer-clojure :exclude [symbol sequence]))
 
-(defn sin [x] (Math/sin x))
-(defn cos [x] (Math/cos x))
-(defn tan [x] (Math/tan x))
+;; Number/symbol tokens
 
 (defn number [[first & rest]]
   (when (number? first)
@@ -19,7 +17,7 @@
   (when (symbol? first)
     [rest first]))
 
-;; Operators
+;; Operator tokens
 
 (defn pow [[first & rest]]
   (when (= '** first)
@@ -43,7 +41,10 @@
 
 ;; Combinators
 
-(defn sequence [& parsers]
+(defn sequence
+  "Parses an A followed by a B (followed by a ...).
+   Succeeds if all parsers succeed, and returns all the outputs concatenated together"
+  [& parsers]
   #(loop [in %
           [p & more] parsers
           result []]
@@ -55,38 +56,71 @@
                 more
                 (conj result r))))))
 
-(defn choice [& parsers]
+(defn choice
+  "Parses an A or if that fails, a B (or if that fails a ...).
+   Returns the output of the first parses to succeed (or nil)"
+  [& parsers]
   (fn [in] (some #(% in) parsers)))
 
-(defn option [default parser]
-  #(or (parser %)
-       [% default]))
+(defn option
+  "Parses either an A, or successfully parses nothing.
+   Returns either the output of A, or the default value with no input consumed"
+  ([parser]
+     (option nil parser))
+  ([default parser]
+     #(or (parser %)
+          [% default])))
 
-(defn repetition [min parser]
-  #(loop [in %
-          result []]
-     ;;(prn "rep" in)
-     (let [rr (parser in)]
-       ;;(prn "->" rr)
-       (if-let [[inp r] rr]
-         (recur inp
-                (conj result r))
-         (when (>= (count result) min)
-           [in result])))))
+(defn repetition
+  "Parse a series of at least min 'A's.
+   Returns a sequence of min or more A outputs, or nil"
+  ([parser]
+     (repetition 0 parser))
+  ([min parser]
+     #(loop [in %
+             result []]
+        (if-let [[inp r] (parser in)]
+          (recur inp
+                 (conj result r))
+          (when (>= (count result) min)
+            [in result])))))
 
-(defn action [f parser]
+(defn action
+  "Apply a function to the result of another parser."
+  [func parser]
   #(when-let [[in r] (parser %)]
-     [in (f r)]))
+     [in (func r)]))
+
+;; Actions
+
+(defn collapse-signs
+  "Collapse a series of unary +/- signs into zero or one - signs"
+  [[signs expr]]
+  (if (odd? (count (filter (partial = '-) signs)))
+    (list '- expr)
+    expr))
+
+(defn right-assoc
+  [[first [[operator second] & more]]]
+  (if second
+    (list operator first (right-assoc [second more]))
+    first))
+
+(defn left-assoc
+  [[first [[operator second] & more]]]
+  (if second
+    (recur [(list operator first second) more])
+    first))
 
 ;; Grammar
 
-(declare add-expr)
+(declare expression)
 
 (defn paren
   "A parenthesised sequence of zero or more expressions"
   [[first & rest]]
   (when (list? first)
-    (let [[in r] ((repetition 0 add-expr) first)]
+    (let [[in r] ((repetition expression) first)]
       ;; TODO: (assert (not (seq in)))
       [rest r])))
 
@@ -98,64 +132,58 @@
       [in (first r)])))
 
 (def function
-  (action (fn f [[name args]]
-            (cons name args))
+  "Function application (a symbol followed by a parenthesised list of expressions)"
+  (action (partial apply cons)
           (sequence symbol paren)))
 
 (def simple
-  (action (fn [[signs num]]
-            (if (odd? (count (filter (partial = '-) signs)))
-              (list '- num)
-              num))
-          (sequence (repetition 0 (choice add sub))
+  "A single function, symbol, number or parenthesised expression,
+   preceded by zero or more unary + and/or - operators"
+  (action collapse-signs
+          (sequence (repetition (choice add sub))
                     (choice function symbol number parenthesised))))
 
-(def pow-expr
-  (action (fn f [[a [[s b] & c]]]
-            (if b
-              (list s a (f [b c]))
-              a))
+(def factor
+  (action right-assoc
           (sequence simple
-                    (repetition 0 (sequence pow simple)))))
+                    (repetition (sequence pow simple)))))
 
-(def mul-expr
-  (action (fn f [[a [[s b] & c]]]
-            (if b
-              (f [(list s a b) c])
-              a))
-          (sequence pow-expr
-                    (repetition 0 (sequence (choice mul div) pow-expr)))))
+(def term
+  (action left-assoc
+          (sequence factor
+                    (repetition (sequence (choice mul div) factor)))))
 
-(def add-expr
-  (action (fn f [[a [[s b] & c]]]
-            (if b
-              (f [(list s a b) c])
-              a))
-          (sequence mul-expr
-                    (repetition 0 (sequence (choice add sub) mul-expr)))))
+(def expression
+  (action left-assoc
+          (sequence term
+                    (repetition (sequence (choice add sub) term)))))
 
 ;; Parser
 
 (defn parse [s & [parser]]
   (apply prn "Parsing:" s)
-  (let [f (or parser add-expr)
-        r (f s)]
-    (if-let [[rem res] r]
-      (do (when (seq rem)
-            (prn "Oops:" rem)) ;; TODO: Make this an error
-          (prn "Result:" res)
-          res)
-      (prn "Fail"))))
+  (if-let [[rem res] ((or parser expression) s)]
+    (do (when (seq rem)
+          (prn "Oops:" rem)) ;; TODO: Make this an error
+        (prn "Result:" res)
+        res)
+    (prn "Fail")))
+
+;; Macro
 
 (defmacro $= [& tokens]
   (parse tokens))
 
-(defn foo [& m]
-  (clojure.string/join "," m))
+;; Tests/examples
+
+(defn sin [x] (Math/sin x))
 
 (prn "A:" ($= sin(3.14) ))
 
-(prn "B:" ($= foo((2 + 1.9) / 25, 3 ** (1 / 0.7))))
+(defn foo [& m]
+  (clojure.string/join "," m))
+
+(prn "B:" ($= foo((2 + 1.9) / 25, 3 ** (1 / 0.7), 1 + - + -1)))
 
 (def a 6)
 (def b 23)
@@ -166,4 +194,5 @@
 ;; TODO:
 ;; - check whole input is consumed
 ;; - better error handling
+;; - more operators: bitwise/logical/other?
 ;; - handle missing spaces (where possible): 3 *2 or -a * 4 (but 3*2 can't work)
